20eaca2440d73528c5455e74fc21f847dcfd1045
Adel <adeltempname@gmail.com>
2017-11-18 17:58:19 +0100

    module1

---
+++ module1.C/cardio.h
@@ -0,0 +1,7 @@
+#ifndef CARDIO_H_INCLUDED
+#define CARDIO_H_INCLUDED
+
+int verifcardio(int val, int seuil, int valeurPrecedente);
+int calculcardio(int tempsDetection, int previous, int seuil);
+
+#endif // CARDIO_H_INCLUDED
